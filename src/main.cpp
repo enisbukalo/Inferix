e77b2ce470c99002cdfa9c1b470e7441a6b27bcb
Enis Bukalo <bukaloenis@gmail.com>
2026-02-26 21:57:14 -0500

    Moving files around

--- src/main.cpp
+++ src/main.cpp
@@ -2,35 +2,46 @@
 
 #include "httplib.h"
 #include "json.hpp"
+#include "ram_monitor.hpp"
 
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/dom/elements.hpp>
+#include <ftxui/dom/table.hpp>
 #include <string>
 
 using namespace ftxui;
 
 int main(){
+	MemoryMonitor::instance().update();
+	auto ram_stats = MemoryMonitor::instance().get_stats();
 	auto screen = ScreenInteractive::Fullscreen();
 
-	// State
 	int left_size = 20;
 	int right_size = 20;
 	int top_size = 10;
 	int bottom_size = 10;
 
-	// Renderers
-	auto RendererInfo = [](const std::string& name, int* size){
-		return Renderer([name, size]{
-				return text(name + ": " + std::to_string(*size)) | center;
-				});
-	};
 	auto middle = Renderer([] {return text("Middle") | center;});
-	auto left = RendererInfo("Left", &left_size);
-	auto right = RendererInfo("Right", &right_size);
-	auto top = RendererInfo("Top", &top_size);
-	auto bottom = RendererInfo("Bottom", &bottom_size);
+	auto left = Renderer([&]() { return text("Left: " + std::to_string(left_size)) | center;});
+	auto right = Renderer([&]() { return text("Right: " + std::to_string(right_size)) | center;});
+	auto top = Renderer([&]() {
+		auto table = Table({
+			{"Metric", "Value"},
+			{"Total", std::to_string(ram_stats.total_bytes)},
+			{"Used", std::to_string(ram_stats.used_bytes)},
+			{"Available", std::to_string(ram_stats.available_bytes)},
+			{"Usage", std::to_string(ram_stats.usage_percentage) + "%"},
+		});
+		table.SelectColumn(0).Decorate(color(Color::MagentaLight));
+		table.SelectColumn(0).Border(ftxui::DASHED);
+		table.SelectColumn(1).Decorate(color(Color::LightGreen));
+		table.SelectRow(0).Decorate(bold);
+		table.SelectAll().Border(ftxui::DOUBLE);
+		return table.Render() | frame | border;
+	});
+	auto bottom = Renderer([&]() { return text("Bottom: " + std::to_string(bottom_size)) | center;});
 
 	auto container = middle;
 	container = ResizableSplitLeft(left, container, &left_size);
@@ -38,10 +49,7 @@ int main(){
 	container = ResizableSplitTop(top, container, &top_size);
 	container = ResizableSplitBottom(bottom, container, &bottom_size);
 
-	auto renderer =
-	    Renderer(container, [&] { return container->Render() | border; });
-
-	screen.Loop(renderer);
+	screen.Loop(container);
 
 	return 0;
 }
