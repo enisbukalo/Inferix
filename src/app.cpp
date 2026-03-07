72e0c2171908405bf5bb90e391848700bbe9bf61
Enis Bukalo <bukaloenis@gmail.com>
2026-03-07 10:56:12 -0500

    Updating Layout

--- src/app.cpp
+++ src/app.cpp
@@ -29,21 +29,23 @@ void App::Run()
 	auto screen = ScreenInteractive::Fullscreen();
 	SystemMonitorRunner runner(screen);
 
-	std::vector<std::string> tab_values{ "Settings", "OpenCode" };
+	std::vector<std::string> tab_values{ "Settings", "Server Log", "Terminal" };
 	int selected_tab = 0;
 	auto tab_toggle = Toggle(&tab_values, &selected_tab);
 
 	auto settings_content = Renderer([] {
 		return window(text(""),
-					  hbox({ separatorEmpty(),
-							 vbox({ ModelsPanel::Render(),
-									ModelPresetsPanel::Render() }) |
-								 flex,
-							 separatorEmpty(),
-							 vbox({ LoadSettingsPanel::Render(),
-									InferenceSettingsPanel::Render() }) |
-								 flex,
-							 separatorEmpty() })) |
+					  hbox({
+						  vbox({ ModelsPanel::Render(),
+								 ModelPresetsPanel::Render() }) |
+							  flex,
+						  filler(),
+						  // separatorEmpty(),
+						  vbox({ LoadSettingsPanel::Render(),
+								 InferenceSettingsPanel::Render() }) |
+							  flex,
+					  }),
+					  ftxui::EMPTY) |
 			   flex;
 	});
 
@@ -54,18 +56,24 @@ void App::Run()
 					  ServerInfoPanel::Render() });
 	});
 
-	auto opencode_content =
-		Renderer([] { return window(text("") | bold, text("")) | flex; });
+// Placeholder for when we implement a Terminal
+	auto terminal_content =
+		Renderer([] { return window(text(""), text(""), ftxui::EMPTY) | flex; });
 
-	auto tab_container =
-		Container::Tab({ settings_content, opencode_content }, &selected_tab);
+// Placeholder for when we implement reading live log outputs from llama.cpp
+	auto log_output_content =
+		Renderer([] { return window(text(""), text(""), ftxui::EMPTY) | flex; });
+
+	auto tab_container = Container::Tab(
+		{ settings_content, log_output_content, terminal_content },
+		&selected_tab);
 
 	auto interactive = Container::Vertical({ tab_toggle, tab_container }) |
 					   borderRounded | flex;
 
 	auto container = Renderer(interactive, [&] {
 		return vbox({ SystemResourcesPanel::Render(),
-					  separatorHeavy(),
+					  separatorCharacter("*") | bold | color(Color::Orange3),
 					  interactive->Render(),
 					  server_content->Render() }) |
 			   flex;
