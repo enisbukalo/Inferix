2e4795151453362f524193ea039f7b276d9bb7bb
Enis Bukalo <bukaloenis@gmail.com>
2026-04-25 09:45:39 -0400

    Formatting and bug fix

--- include/core/tabLayout.h
+++ include/core/tabLayout.h
@@ -15,14 +15,12 @@
 
 #pragma once
 
+#include <ftxui/component/component.hpp>
+
 #include <string>
 #include <vector>
 
 class TerminalCoordinator;
-
-namespace ftxui {
-class Component;
-} // namespace ftxui
 
 /**
  * @brief Assembles the complete UI layout: SystemResourcesPanel + separator +
